097dd049c6906c70ffb532179fcd912e5af18e04
Lexwomy <13265459+Lexwomy@users.noreply.github.com>
2024-07-30 00:32:23 -0500

    Initial commit

---
+++ src/main/java/lexwomy/fletchingtable/FletchingTableDataGenerator.java
@@ -0,0 +1,11 @@
+package lexwomy.fletchingtable;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FletchingTableDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/lexwomy/fletchingtable/FletchingTableInitializer.java
@@ -0,0 +1,22 @@
+package lexwomy.fletchingtable;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FletchingTableInitializer implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("fletching-table");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
