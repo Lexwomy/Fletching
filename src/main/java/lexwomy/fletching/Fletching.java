ee315135acbbfbf7f840789e44ca30c1122b29f2
Lexwomy <13265459+Lexwomy@users.noreply.github.com>
2024-08-12 14:27:25 -0500

    Added crippling, focus, and hunter enchantments

--- src/main/java/lexwomy/fletching/Fletching.java
+++ src/main/java/lexwomy/fletching/Fletching.java
@@ -1,6 +1,7 @@
 package lexwomy.fletching;
 
 import lexwomy.fletching.component.FletchingComponents;
+import lexwomy.fletching.effect.FocusEffect;
 import lexwomy.fletching.effect.FrenzyEffect;
 import lexwomy.fletching.item.FletchingItems;
 import lexwomy.fletching.screen.FletchingScreenHandler;
@@ -9,6 +10,7 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
@@ -33,8 +35,10 @@ public class Fletching implements ModInitializer {
 	public static final StatusEffect FRENZY_EFFECT = Registry.register(Registries.STATUS_EFFECT, Identifier.of(MOD_ID, "frenzy"),
 			new FrenzyEffect().addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, Identifier.of(Fletching.MOD_ID, "frenzy"),
 					0.02F, EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+	public static final StatusEffect FOCUS_EFFECT = Registry.register(Registries.STATUS_EFFECT, Identifier.of(MOD_ID, "focus"),
+			new FocusEffect());
 	public static final RegistryEntry<StatusEffect> FRENZY = Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(MOD_ID, "frenzy"), FRENZY_EFFECT);
-
+	public static final RegistryEntry<StatusEffect> FOCUS = Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(MOD_ID, "focus"), FOCUS_EFFECT);
 	@Override
 	public void onInitialize() {
 		//Registers the fletching screen handler to the minecraft registries
